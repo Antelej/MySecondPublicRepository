63776e8c460a32666c3cb1962610abeed20f81c4
Antelej <75899058+Antelej@users.noreply.github.com>
2020-12-13 01:59:25 +0300

    Add files via upload

---
+++ Scripts/FPSCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+    public Transform playerBody;
+    public Transform playerHead;
+
+    //iniate 2 fields for body and head
+
+    private float _xRotation = 0f;
+    private float _yRotation = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //fixing the center of screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //user input
+        
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        _yRotation -= mouseX;
+        _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+
+        //restrictioin for user input
+
+
+
+        playerHead.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX );
+        playerHead.Rotate(Vector3.forward * mouseY);
+
+        //use quaternion for rotation of body and head
+         
+         
+
+
+    }
+}
---
+++ Scripts/FPSMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 120.0f;
+
+    // initiate public variable the sensivity of speed
+
+    private Rigidbody _rigidbody;
+
+    
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        // user input
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        _rigidbody.MovePosition(transform.position + move.normalized * speed * Time.deltaTime);
+
+        // the elementary code for transformation
+    }
+}
